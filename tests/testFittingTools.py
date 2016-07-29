9d5098d11a47bbe508c785c5beb5f8da5845b0b1
Gavin Scot Stewart <gavart@linux2.cs.ox.ac.uk>
2016-07-29 18:01:46 +0100

    CMA-ES works when E_0, k_0 are given, but fails otherwise

--- tests/testFittingTools.py
+++ tests/testFittingTools.py
@@ -4,29 +4,126 @@ import tools.dataFileIO as dfio
 import numpy as np
 import unittest
 import matplotlib.pyplot as plt
+import copy
 
-class FittingToolsTestCase(unittest.TestCase):
+class FittingToolsMaxInformationTestCase(unittest.TestCase):
+    """"
+    Give the fitting procedure the true parameters as a starting point.
+    """
 
-    def testCapacitanceAndFrequencyFitFunctionMatchesActual(self):
+    @classmethod
+    def setUpClass(cls):
         # Read in data
 	fileName = "./files/simulationParameters.json"
 	dataName = "Martin's experiment"
-	params = dfio.readParametersFromJSON(fileName, dataName)
-	tEnd = 2 * (params["ERev"] - params["EStart"]) / params["nu"]
-	t = np.linspace(0, tEnd, int(np.ceil(tEnd * params["freq"] * 200)))
-	IObs, _ = st.solveIFromJSON(t, params)
-	exp = ft.ACExperiment(IObs, params["Ru"], params["temp"], 
-                      params["area"], params["coverage"], params["nu"], 
-                      params["freq"], params["dE"], params["EStart"], 
-                      params["ERev"], tEnd, params["reverse"])
-	exp.fitCapacitanceAndFrequency(params["Cdl"], params["Cdl1"], params["Cdl2"], params["Cdl3"])
-	
-        plt.figure(1)
-        plt.plot(exp.t, exp.IObs)
+	cls.params = dfio.readParametersFromJSON(fileName, dataName)
+	cls.tEnd = 2 * (cls.params["ERev"] - cls.params["EStart"]) / cls.params["nu"]
+	cls.t = np.linspace(0, cls.tEnd, int(np.ceil(cls.tEnd * cls.params["freq"] * 200)))
+	cls.IObs, _ = st.solveIFromJSON(cls.t, cls.params)
 
-	self.assertAlmostEqual(exp.Cdl, params["Cdl"])
-	self.assertAlmostEqual(exp.Cdl1, params["Cdl1"])
-	self.assertAlmostEqual(exp.Cdl2, params["Cdl2"])
-	self.assertAlmostEqual(exp.Cdl3, params["Cdl3"])
-	self.assertAlmostEqual(exp.freq, params["freq"])
-	
+
+    def setUp(self):
+	params = self.params
+        self.exp = ft.ACExperiment(self.IObs, params["Ru"], params["temp"], 
+                      params["area"], params["coverage"], params["nu"], 
+                      9, params["dE"], params["EStart"], 
+                      params["ERev"], self.tEnd, params["reverse"])
+	self.exp.addDebugParam("loud-cma")
+
+    def tearDown(self):
+        pass
+
+    def setE0k0ToCorrect(self):
+	self.exp.E_0 = self.params["E_0"]
+        self.exp.k_0 = self.params["k_0"]
+
+    def setSliceToWholeTime(self):
+        self.exp.noFaradaicSlices = [np.s_[:]]
+        
+
+    def checkMatchesParams(self):
+        exp = self.exp 
+	self.assertAlmostEqual(exp.Cdl, self.params["Cdl"], 7)
+	self.assertAlmostEqual(exp.Cdl1, self.params["Cdl1"], 7)
+	self.assertAlmostEqual(exp.Cdl2, self.params["Cdl2"], 7)
+	self.assertAlmostEqual(exp.Cdl3, self.params["Cdl3"], 8)
+	self.assertAlmostEqual(exp.freq, self.params["freq"], 4)
+
+    def testFitFunctionMatchesActualObjectiveOverWholeTimeWithRealParameters(self):
+	exp = self.exp
+	params = self.params
+        exp.freq = params["freq"]
+        self.setSliceToWholeTime()
+        self.setE0k0ToCorrect()
+        exp.fitCapacitanceAndFrequency(params["Cdl"], params["Cdl1"], params["Cdl2"], params["Cdl3"])
+
+        self.checkMatchesParams()
+
+    def testFitFunctionMatchesActualObjectiveOverWholeTimeOnlyCapacitanceGiven(self):
+	exp = self.exp
+	params = self.params
+        self.setSliceToWholeTime()
+        self.setE0k0ToCorrect()
+        exp.addDebugParam("No-freq-preprocessing")
+        exp.fitCapacitanceAndFrequency(params["Cdl"], params["Cdl1"], params["Cdl2"], params["Cdl3"])
+
+        self.checkMatchesParams()
+
+    def testFitMatchesActualOverWholeTimeNothingGiven(self):
+	exp = self.exp
+	params = self.params
+        self.setSliceToWholeTime()
+        self.setE0k0ToCorrect()
+        exp.addDebugParam("No-freq-preprocessing")
+        exp.fitCapacitanceAndFrequency()
+
+        self.checkMatchesParams()
+
+    def testFitMatchesActualNormalSliceParametersGiven(self):
+	exp = self.exp
+	params = self.params
+        exp.freq = params["freq"]
+        self.setE0k0ToCorrect()
+        exp.fitCapacitanceAndFrequency(params["Cdl"], params["Cdl1"], params["Cdl2"], params["Cdl3"])
+
+        self.checkMatchesParams()
+
+    def testFitFunctionMatchesActualNormalSliceOnlyCapacitanceGiven(self):
+	exp = self.exp
+	params = self.params
+        self.setE0k0ToCorrect()
+        exp.addDebugParam("No-freq-preprocessing")
+        exp.fitCapacitanceAndFrequency(params["Cdl"], params["Cdl1"], params["Cdl2"], params["Cdl3"])
+
+        self.checkMatchesParams()
+
+    def testFitMatchesActualOverNormalSliceNothingGiven(self):
+	exp = self.exp
+	params = self.params
+        self.setE0k0ToCorrect()
+        exp.fitCapacitanceAndFrequency()
+
+        self.checkMatchesParams()
+
+    def testFitMatchesActualNormalSliceParametersGivenNoE0K0(self):
+	exp = self.exp
+	params = self.params
+        exp.freq = params["freq"]
+        exp.fitCapacitanceAndFrequency(params["Cdl"], params["Cdl1"], params["Cdl2"], params["Cdl3"])
+
+        self.checkMatchesParams()
+
+    def testFitFunctionMatchesActualNormalSliceOnlyCapacitanceGivenNoE0K0(self):
+	exp = self.exp
+	params = self.params
+        exp.addDebugParam("No-freq-preprocessing")
+        exp.fitCapacitanceAndFrequency(params["Cdl"], params["Cdl1"], params["Cdl2"], params["Cdl3"])
+
+        self.checkMatchesParams()
+
+    def testFitMatchesActualOverNormalSliceNothingGivenNoE0K0(self):
+	exp = self.exp
+	params = self.params
+        exp.fitCapacitanceAndFrequency()
+
+        self.checkMatchesParams()
--- tools/fittingTools.py
+++ tools/fittingTools.py
@@ -5,62 +5,110 @@ A module containing functions used to fit parameters to data.
 import numpy as np
 import cma
 import solutionTools as st
+import matplotlib.pyplot as plt
 
 class ACExperiment(object):
     """
     A class to contain experimental data.
     """
+
+    #TODO: Make these object level?
+    CdlRescale = 1e4
+    Cdl1Rescale = 1.67e3
+    Cdl2Rescale = 5e3
+    Cdl3Rescale = 1e6
+    freqRescale = 2e2
 	
     def __init__(self, IObs, Ru, temp, area, coverage, nu, freq, dE, EStart,
                 ERev, tEnd, reverse = True):
         self.IObs = np.ravel(IObs)
-	self.Ru = Ru
-	self.temp = temp
-	self.area = area
-	self.coverage = coverage
-	self.nu = nu
-	self.Cdl = None
-	self.Cdl1 = None
-	self.Cdl2 = None
-	self.Cdl3 = None
-	self.freq = freq
-	self.dE = dE
-	self.EStart = EStart
-	self.ERev = ERev
-	self.reverse = reverse
-	self.t = np.linspace(0, tEnd, len(self.IObs))
+        self.Ru = Ru
+        self.temp = temp
+        self.area = area
+        self.coverage = coverage
+        self.nu = nu
+        self.Cdl = None
+        self.Cdl1 = None
+        self.Cdl2 = None
+        self.Cdl3 = None
+        self.dE = dE
+        self.EStart = EStart
+        self.ERev = ERev
+        self.reverse = reverse
+        self.t = np.linspace(0, tEnd, len(self.IObs))
+        self.E_0 = 0.5 * (EStart + ERev)
+        self.k_0 = 0.0
+	self.debugParams = []
+        n = len(IObs)
+        self._freqPreprocessing()
+        self.noFaradaicSlices = []
+	if reverse:
+           self.noFaradaicSlices.append(np.s_[:n / 10])
+           self.noFaradaicSlices.append(np.s_[4 * n / 10: 6 * n / 10])
+           self.noFaradaicSlices.append(np.s_[9 * n / 10:])
+        else:
+           self.noFaradaicSlices.append(np.s_[:n / 5])
+           self.noFaradaicSlices.append(np.s_[4 * n / 5:])
 
-    #TODO add a factory method to make an ACExperiment for data contained in 
+    def addDebugParam(self, param):
+        """add a parameter to be used for debugging"""
+        self.debugParams.append(param)
+
+    #TODO add a factory method to make an ACExperiment from data contained in 
     #     a JSON file.
 
     def _capacitanceAndFreqObjectiveFunction(self, capFreqParams):
-        Cdl, Cdl1, Cdl2, Cdl3, freq = capFreqParams #Unpack
-	# Assume E_0 in the middle of sweep
-        E_0 = 0.5 * (self.EStart + self.ERev) 
-	k_0 = 0. #No Faradaic current
-        # TODO: Implement box constraints to make resampling unneeded.
+        Cdl, Cdl1, Cdl2, Cdl3, freq = self._unscaleCapFreqParams(capFreqParams)
 	try:
-            I, _ = st.solveIDimensional(self.t, E_0, self.dE, freq, k_0, 
+            I, _ = st.solveIDimensional(self.t, self.E_0, self.dE, freq, self.k_0, 
                              self.Ru, Cdl, Cdl1, Cdl2, Cdl3, self.EStart, 
                              self.ERev, self.temp, self.nu, self.area, 
                              self.coverage, self.reverse)
 
         except st.ConvergenceError:
             return np.nan
-        n = len(self.IObs)
-        if self.reverse:
-            n_l = n / 20
-            n_h =  9 * n / 20
-            n_p = n / 2
-            return np.sqrt(
-                  np.sum(np.square(self.IObs[:n_l] - I[:n_l])) +
-                  np.sum(np.square(self.IObs[n_h:n_l+n_p] - I[n_h:n_l+n_p])) +
-                  np.sum(np.square(self.IObs[n_h+n_p:] - I[n_h+n_p])))
+	retSum = 0.0
+	for s in self.noFaradaicSlices:
+            retSum += np.sum(np.square(self.IObs[s] - I[s]))
+	return retSum
+
+    def _rescaleCapFreqParams(self, paramList):
+        Cdl, Cdl1, Cdl2, Cdl3, freq = paramList
+        return [Cdl * self.CdlRescale,
+                Cdl1 * self.Cdl1Rescale,
+                Cdl2 * self.Cdl2Rescale,
+                Cdl3 * self.Cdl3Rescale,
+                (freq - self.freq) * self.freqRescale]
+
+    def _unscaleCapFreqParams(self, paramList):
+        CdlRescaled, Cdl1Rescaled, Cdl2Rescaled, Cdl3Rescaled, freqRescaled = paramList
+        return [CdlRescaled / self.CdlRescale,
+                Cdl1Rescaled / self.Cdl1Rescale,
+                Cdl2Rescaled / self.Cdl2Rescale,
+                Cdl3Rescaled / self.Cdl3Rescale,
+                self.freq + freqRescaled / self.freqRescale]
+
+    def _freqPreprocessing(self):
+        """
+        approximate the frequency by taking the average distance between 
+        the peaks in IObs.
+        """
+        tPeak, _ = st.extractPeaks(self.t, self.IObs)
+        if len(tPeak) < 2:
+            pass
         else:
-            n_l = n / 10
-            n_h = 9 * n / 10
-            return np.sqrt(np.sum(np.square(self.IObs[:n_l] - I[:n_l])) + 
-                           np.sum(np.square(self.IObs[n_h:] - I[n_h:])))
+            self.freq = 1.0 / np.mean(np.diff(tPeak))
+
+    def plotFitDiscrepancy(self):
+        """
+        plot the discrepency between the fitted values and observations.
+        """
+        IFit, _ = st.solveIDimensional(self.t, self.E_0, self.dE, self.freq, self.k_0, 
+                      self.Ru, self.Cdl, self.Cdl1, self.Cdl2, self.Cdl3, self.EStart, 
+                      self.ERev, self.temp, self.nu, self.area, 
+                      self.coverage, self.reverse)
+
+        plt.plot(self.t, abs(self.IObs - IFit))
 
     def fitCapacitanceAndFrequency(self, CdlStart = 0.0, Cdl1Start = 0.0, 
       Cdl2Start = 0.0, Cdl3Start = 0.0):
@@ -68,11 +116,16 @@ class ACExperiment(object):
         Obtains values of the capacitance parameters and frequency using a 
         CMA-ES algorithm.
         """
+        if "no-freq-preprocessing" not in self.debugParams:
+        	self._freqPreprocessing()
         start = [CdlStart, Cdl1Start, Cdl2Start, Cdl3Start, self.freq]
-
-	tf = cma.BoxConstraintsLinQuadTransformation([[-5e-4,5e-4], [-5e-3, 5e-3], [-5e-3, 5e-3], [-1e-4, 1e-4], [self.freq * 0.9, self.freq * 1.1]])
-        res = cma.fmin(self._capacitanceAndFreqObjectiveFunction, start, 1e-2, {'transformation': [tf.transform, tf.inverse], 'tolfun' : 1e-14, 'tolfunhist' : 1e-14})
-        res = res[0]
+        start = self._rescaleCapFreqParams(start)
+	verbose = 1 if "loud-cma" in self.debugParams else -9
+        optDict = {'tolfun' : 1e-14, 'tolfunhist' : 1e-14, "verbose" : verbose}
+        res = cma.fmin(self._capacitanceAndFreqObjectiveFunction, start, 1, 
+                       optDict) 
+        res = self._unscaleCapFreqParams(res[0])
+	print res
 	self.Cdl = res[0]
 	self.Cdl1 = res[1]
 	self.Cdl2 = res[2]
